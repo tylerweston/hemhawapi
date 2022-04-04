081da7a98c2790f7512f01ba01b78f8786170fa1
Tyler Weston <tylerjohnweston@gmail.com>
2022-04-04 17:48:19 -0400

    begin support for separate high score tables in database

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,14 @@
 # hemhaw API
 # Tyler Weston, March 2022
 
+# Database should store:
+#   - User hash
+#   - User name
+#   - high easy score
+#   - high medium score
+#   - high hard score
+#   - high blitz score
+
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask import request
@@ -27,12 +35,16 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = "users"
     hash = db.Column(db.String(32), primary_key=True)
-    username = db.Column(db.String(32), nullable=False)
-    score = db.Column(db.Integer)
+    name = db.Column(db.String(32), nullable=False)
+    total_score = db.Column(db.Integer)
+    easy_score = db.Column(db.Integer)
+    medium_score = db.Column(db.Integer)
+    hard_score = db.Column(db.Integer)
+    blitz_score = db.Column(db.Integer)
 
     def __repr__(self):
         #return '<User {}>'.format(self.username)
-        return f'{self.username} - {self.score}'
+        return f'{self.username} - {self.total_score}'
 
 
 @app.route('/hello')
@@ -55,7 +67,7 @@ def user():
         return 'No hash provided'
     elif request.method == 'POST':
         # Create a new user via user with the supplied username and hash, and empty asides from that
-        user = User(username=request.args.get('name'), hash=request.args.get('hash'), score=0)
+        user = User(name=request.args.get('name'), hash=request.args.get('hash'), score=0)
         db.session.add(user)
         db.session.commit()
         return 'Posted user'
@@ -66,17 +78,68 @@ def score():
     #return f"{username} scored {word}"
     if request.method == 'GET':
         # Return a list of the top 50 scores in database
-        scores = User.query.order_by(User.score.desc()).limit(50)
+        scores = User.query.order_by(User.total_score.desc()).limit(50)
         return '\n'.join([str(score) for score in scores])
 
     if request.method == 'POST':
         # update the score of a user, we should take a hash and a score
         hash = request.args.get('hash')
-        score = request.args.get('score')
+        total_score = request.args.get('score')
+        easy_score = request.args.get('easy_score')
+        medium_score = request.args.get('medium_score')
+        hard_score = request.args.get('hard_score')
+        blitz_score = request.args.get('blitz_score')
         user = User.query.filter_by(hash=hash).first()
-        if user:
-            user.score += int(score)
-            db.session.commit()
-            return 'Updated score'
-        else:
+
+        if not user:
             return 'User not found'
+
+        user.total_score += int(total_score)
+        if easy_score:
+            user.easy_score = int(easy_score)
+        if medium_score:
+            user.medium_score = int(medium_score)
+        if hard_score:
+            user.hard_score = int(hard_score)
+        if blitz_score:
+            user.blitz_score = int(blitz_score)
+        
+        db.session.commit()
+        return 'Updated score'
+
+
+@app.route('/globalposition', methods=['GET'])
+@cross_origin()
+def get_global_position():
+    # get the total number of users in the database
+    hash = request.args.get('hash')
+    user = User.query.filter_by(hash=hash).first()
+
+    if not user:
+        return 'User not found'
+
+    # Order the users by score and get the position of user in that list
+    users = User.query.order_by(User.total_score.desc()).all()
+
+    # # SQL query to get rank of user sorted by their total score
+    # TODO: Test this out and see if it works or not
+    # query = """
+    #     SELECT @rank := @rank + 1 AS rank, u.hash, u.name, u.score
+    #     FROM users u, (SELECT @rank := 0) r
+    #     ORDER BY u.score DESC
+    # """
+
+    # # Execute the query
+    # result = db.engine.execute(query)
+
+    # Get the first row of the result
+
+    position = 1
+    for user in users:
+        if user.hash == hash:
+            break
+        position += 1
+
+    # get the total number of users
+    total_users = User.query.count()
+    return f'{str(position)}/{str(total_users)}'
